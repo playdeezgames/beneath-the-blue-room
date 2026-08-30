265eee0a22d4152d3b7f30aad82e4e83dd9c2e82
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-08-30 05:39:13 -0500

    cleaned up characters model

--- src/Metaphor.Models/Characters/CharactersModel.vb
+++ src/Metaphor.Models/Characters/CharactersModel.vb
@@ -9,12 +9,6 @@ Friend Class CharactersModel
         Me.location = location
     End Sub
 
-    Public ReadOnly Property HasOthers As Boolean Implements ICharactersModel.HasOthers
-        Get
-            Return location.HasOtherCharacters(location.World.Avatar)
-        End Get
-    End Property
-
     Public ReadOnly Property Others As IEnumerable(Of ICharacterModel) Implements ICharactersModel.Others
         Get
             Return location.GetOtherCharacters(location.World.Avatar).Select(AddressOf CharacterModel.Create)
@@ -26,10 +20,6 @@ Friend Class CharactersModel
             Return location.Characters.Select(AddressOf CharacterModel.Create)
         End Get
     End Property
-
-    Public Sub ShowList() Implements ICharactersModel.ShowList
-        location.World.ClearMessages()
-    End Sub
 
     Friend Shared Function Create(entity As ILocation) As ICharactersModel
         Return New CharactersModel(entity)
--- src/Metaphor.Models/Characters/ICharactersModel.vb
+++ src/Metaphor.Models/Characters/ICharactersModel.vb
@@ -1,6 +1,4 @@
 ﻿Public Interface ICharactersModel
-    ReadOnly Property HasOthers As Boolean
     ReadOnly Property Others As IEnumerable(Of ICharacterModel)
     ReadOnly Property All As IEnumerable(Of ICharacterModel)
-    Sub ShowList()
 End Interface
